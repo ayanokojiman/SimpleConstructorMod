0f6437d753c2f7d6332fb211371ada78cdf8475c
ayanokojiman <w1830314@gmail.com>
2025-11-07 20:37:21 +0500

    feat(PlayerUtils): add GetPlayer, GetPlayerPos methods

--- Utils/PlayerUtils.cs
+++ Utils/PlayerUtils.cs
@@ -1,14 +1,26 @@
+using Microsoft.Xna.Framework;
 using Terraria;
 
 namespace SimpleConstructor.Utils
 {
     public static class PlayerUtils
     {
+        public static Player GetPlayer()
+        {
+            return Main.LocalPlayer;
+        }
         public static string GetFacingDirection(Player playerInstance)
         {
             return playerInstance.direction == 1
                 ? "right"
                 : (playerInstance.direction == -1 ? "left" : "neutral");
+        }
+        public static Vector2 GetPlayerPos()
+        {
+            Player p = GetPlayer();
+            int centerX = (int)((p.position.X + p.width / 2f) / 16f);
+            int centerY = (int)((p.position.Y + p.height) / 16f);
+            return new Vector2(centerX, centerY);
         }
     }
 }
